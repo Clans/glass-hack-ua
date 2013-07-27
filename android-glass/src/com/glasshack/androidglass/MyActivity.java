4003b6d34c4f4a719ec0b55a91be591473350cef
unknown <fenixhomepro@gmail.com>
2013-07-27 21:33:24 +0300

    Added bitmap converter

--- android-glass/src/com/glasshack/androidglass/MyActivity.java
+++ android-glass/src/com/glasshack/androidglass/MyActivity.java
@@ -1,6 +1,9 @@
 package com.glasshack.androidglass;
 
+import net.linkpc.fenixhomepro.testapp.R;
+import net.linkpc.fenixhomepro.testapp.SystemUtils;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MyActivity extends BluetoothActivity {
 
@@ -23,12 +26,23 @@ public class MyActivity extends BluetoothActivity {
 	/*
 	 * Write 
 	 * ObjectMapper mapper = new ObjectMapper();
-	 * Entity entity = new
-	 * Entity(4, "Ok, Glass");
+	 * Entity entity = new Entity(4, "Ok, Glass");
+	 * String result = mapper.writeValueAsString(entity);
+	 */
+    
+    /*
+	 * Write Bitmap
+	 * ObjectMapper mapper = new ObjectMapper();
+	 * Entity entity = new Entity(4, SystemUtils.bitmapToString(bitmap));
 	 * String result = mapper.writeValueAsString(entity);
 	 */
 
 	/* Read 
-	 * Entity entityResult = mapper.readValue(resultJsonString, Entity.class);
+	 * Entity entityResult = mapper.readValue(result, Entity.class);
+	 * */
+    
+    /* Read bitmap
+	 * Entity entityResult = mapper.readValue(result, Entity.class);
+	 * ((ImageView) findViewById(R.id.image_parse)).setImageBitmap(SystemUtils.stringToBitmap((String)entityResult.getData())
 	 * */
 }
