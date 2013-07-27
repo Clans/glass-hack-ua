93a428fcb97a6773ddf9f046917d04e81c793366
unknown <fenixhomepro@gmail.com>
2013-07-27 21:12:50 +0300

    Prerape read/write entity

--- android-glass/src/com/glasshack/androidglass/MyActivity.java
+++ android-glass/src/com/glasshack/androidglass/MyActivity.java
@@ -4,12 +4,21 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class MyActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	/*
+	 * Write 
+	 * ObjectMapper mapper = new ObjectMapper();
+	 * Entity entity = new
+	 * Entity(4, "Ok, Glass");
+	 * String result = mapper.writeValueAsString(entity);
+	 */
+
+	/* Read 
+	 * Entity entityResult = mapper.readValue(resultJsonString, Entity.class);
+	 * */
 }
